417185f3b3007661c85eb5119e67657a01739678
Tyler Latimer <trlatimer95@gmail.com>
2022-03-31 18:46:15 -0700

    Created Category model and setup connection to database

---
+++ AbbyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AbbyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbbyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
